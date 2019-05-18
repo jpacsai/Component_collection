c3f24a0d8a3d349f78284f72389c06c1611c91d4
jpacsai <judit.pacsai@gmail.com>
2019-05-19 00:17:42 +0200

    add BasicSlider

---
+++ src/components/sliders/BasicSlider.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './BasicSlider.scss';
+
+export type BasicSliderProps = {
+  photos: string[];
+}
+
+class BasicSlider extends React.PureComponent<BasicSliderProps> {
+  static defaultProps = {
+    photos: [
+      '/assets/cats/cat_01.jpg',
+      '/assets/cats/cat_02.jpg',
+      '/assets/cats/cat_03.jpg',
+      '/assets/cats/cat_04.jpg',
+      '/assets/cats/cat_05.jpg'
+    ]
+  }
+
+  render() {
+    const { photos } = this.props;
+
+    const settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      lazyLoad: 'progressive' as any
+    };
+
+    return (
+      <div className="BasicSlider">
+        <Slider {...settings} className='slider'>
+          {photos.map((photo, i) => (
+            <div key={i} className='image'>
+              <img src={process.env.PUBLIC_URL + photo} alt="cat"/>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
+}
+
+export default BasicSlider;
--- src/components/sliders/SliderPage.tsx
+++ src/components/sliders/SliderPage.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Page from '../app-common/Page';
+import Section from '../app-common/Section';
+import BasicSlider from './BasicSlider';
 import './SliderPage.scss';
 
 class SliderPage extends React.PureComponent {
   render() {
     return (
       <Page className="SliderPage" title="Sliders">
+        <Section title="react-slick">
+          <BasicSlider />
+        </Section>
       </Page>
     );
   }
